dcd3fab6d489d7b6a991242c6182266737f65929
Philipp Stephani <p.stephani2@gmail.com>
2014-07-20 20:51:04 +0200

    add license information

--- dup/types.go
+++ dup/types.go
@@ -1,3 +1,12 @@
+// Written in 2014 by Philipp Stephani <p.stephani2@gmail.com>.
+//
+// To the extent possible under law, the author has dedicated all copyright and
+// related and neighboring rights to this software to the public domain worldwide.
+// This software is distributed without any warranty.
+//
+// You should have received a copy of the CC0 Public Domain Dedication along with
+// this software.  If not, see http://creativecommons.org/publicdomain/zero/1.0/.
+
 package dup
 
 type (
