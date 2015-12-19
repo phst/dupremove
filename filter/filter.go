79eb2adb174d68dd55e3adf08b28b1c5c7346bc7
Philipp Stephani <phst@google.com>
2015-12-19 21:25:11 +0100

    Switch to Apache license

--- filter/filter.go
+++ filter/filter.go
@@ -1,11 +1,16 @@
-// Written in 2014 by Philipp Stephani <p.stephani2@gmail.com>.
+// Copyright 2014 Philipp Stephani <phst@google.com>
 //
-// To the extent possible under law, the author has dedicated all copyright and
-// related and neighboring rights to this software to the public domain worldwide.
-// This software is distributed without any warranty.
+// Licensed under the Apache License, Version 2.0 (the "License"); you may not
+// use this file except in compliance with the License.  You may obtain a copy
+// of the License at
 //
-// You should have received a copy of the CC0 Public Domain Dedication along with
-// this software.  If not, see http://creativecommons.org/publicdomain/zero/1.0/.
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+// WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  See the
+// License for the specific language governing permissions and limitations
+// under the License.
 
 package filter
 
